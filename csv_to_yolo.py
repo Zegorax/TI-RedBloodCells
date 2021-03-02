ea164d95c3abc2f19c6f7ad12ace3553c63201dd
Luca Verardo <luca@verardo.ch>
2021-03-02 10:09:32 +0000

    Added dataset in YOLO format

--- csv_to_yolo.py
+++ csv_to_yolo.py
@@ -1,27 +1,12 @@
 import csv
 from PIL import Image
-
-
-reader = csv.DictReader(open('dataset/annotations.csv'))
-annot_dict = []
-
-for row in reader:
-	annot_dict.append(row)
-
-
-for annotation in annot_dict:
-	image = Image.open("dataset/images/" + annotation['image'])
-	width, height = image.size
-	imgSize = [width, height]
-	
-
-
+import os
 
 def BndBox2YoloLine(imgSize, box, classList=[]):
-	xmin = box['xmin']
-	xmax = box['xmax']
-	ymin = box['ymin']
-	ymax = box['ymax']
+	xmin = float(box['xmin'])
+	xmax = float(box['xmax'])
+	ymin = float(box['ymin'])
+	ymax = float(box['ymax'])
 
 	xcen = float((xmin + xmax)) / 2 / imgSize[1]
 	ycen = float((ymin + ymax)) / 2 / imgSize[0]
@@ -30,10 +15,36 @@ def BndBox2YoloLine(imgSize, box, classList=[]):
 	h = float((ymax - ymin)) / imgSize[0]
 
 	# PR387
-	boxName = box['name']
+	boxName = box['label']
 	if boxName not in classList:
 		classList.append(boxName)
 
 	classIndex = classList.index(boxName)
 
-	return classIndex, xcen, ycen, w, h
+	return str(classIndex), str(xcen), str(ycen), str(w), str(h)
+
+
+
+reader = csv.DictReader(open('dataset/annotations.csv'))
+raw_annot_dict = []
+
+for row in reader:
+	raw_annot_dict.append(row)
+
+
+classList = ['rbc', 'wbc']
+
+yolo_dict = []
+for annotation in raw_annot_dict:
+	file_base = "dataset/images/"
+	image = Image.open(file_base + annotation['image'])
+	width, height = image.size
+	imgSize = [width, height]
+	
+	yolo_format = BndBox2YoloLine(imgSize, annotation, classList)
+	
+	f = open(file_base + os.path.splitext(annotation['image'])[0] + ".txt", "a")
+	f.write(" ".join(yolo_format) + "\n")
+	f.close()
+
+
