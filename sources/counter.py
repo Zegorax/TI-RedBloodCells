bf83143113888bbf8aafc0362db64b336b1bf3c8
julien.dossanto <julien.dossantosferreira@he-arc.ch>
2021-03-30 11:10:42 +0200

    Done: méthode classie de détéction de contours

--- sources/counter.py
+++ sources/counter.py
@@ -4,21 +4,24 @@ from skimage.morphology import watershed
 from scipy import ndimage
 import numpy as np
 import cv2
-import skimage.io as sk
 import matplotlib.pyplot as plt
-
+import sys
 
 
 def show(img):
-  plt.imshow(img)
-  plt.show()
+    plt.imshow(img)
+    plt.show()
+
 
 if __name__ == "__main__":
-    img = cv2.imread('rowing.jpeg')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow("Image", img)
+    if(len(sys.argv) <= 1):
+        print("Error bad args!")
+        exit(0)
 
-    #preprocessing the image
+    img = cv2.imread(sys.argv[1])
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # preprocessing the image
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     show(hsv)
 
@@ -31,22 +34,21 @@ if __name__ == "__main__":
     blur = cv2.medianBlur(thr, 5)
     show(blur)
 
-    contours, hierarchy = cv2.findContours(blur,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(
+        blur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     count = []
     for x in contours:
         area = cv2.contourArea(x)
-        if area > 1000 :
+        if area > 1000:
             count.append(x)
-
-    
 
     D = ndimage.distance_transform_edt(thr)
     localMax = peak_local_max(D, indices=False, min_distance=40, labels=thr)
     markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
     labels = watershed(-D, markers, mask=thr)
-    ws = len(np.unique(labels)) -1
+    ws = len(np.unique(labels)) - 1
 
     ans = int((ws + len(count)) / 2)
     print("number of blood cells segments detected = ", ans)
-    cv2.drawContours(img, count, -1, (255,0,0), 3)
+    cv2.drawContours(img, count, -1, (255, 0, 0), 3)
     show(img)
